f22049d01aa0b95b4f6c522a20dd593741766c40
Bitaxa666 <repcity@yandex.ru>
2016-11-10 23:27:59 +0200

    final layout

--- PriceCalculator/gen/edu/example/pricecalculator/R.java
+++ PriceCalculator/gen/edu/example/pricecalculator/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050051;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1102,6 +1102,15 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText1=0x7f05003e;
+        public static final int editText2=0x7f050043;
+        public static final int editText3=0x7f050044;
+        public static final int editText4=0x7f050045;
+        public static final int editText5=0x7f050047;
+        public static final int editText6=0x7f050048;
+        public static final int editText7=0x7f050049;
+        public static final int editText8=0x7f05004e;
+        public static final int editText9=0x7f050050;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1130,6 +1139,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int seekBar1=0x7f05004b;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1137,12 +1147,16 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int tableRow0=0x7f05003d;
-        public static final int tableRow1=0x7f05003e;
-        public static final int tableRow2=0x7f05003f;
-        public static final int tableRow3=0x7f050040;
-        public static final int tableRow4=0x7f050041;
-        public static final int tableRow5=0x7f050042;
+        public static final int textView1=0x7f05003d;
+        public static final int textView10=0x7f05004f;
+        public static final int textView2=0x7f05003f;
+        public static final int textView3=0x7f050040;
+        public static final int textView4=0x7f050041;
+        public static final int textView5=0x7f050042;
+        public static final int textView6=0x7f050046;
+        public static final int textView7=0x7f05004a;
+        public static final int textView8=0x7f05004c;
+        public static final int textView9=0x7f05004d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1248,7 +1262,12 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int billTotal=0x7f0a0010;
+        public static final int custom=0x7f0a0013;
         public static final int hello_world=0x7f0a000e;
+        public static final int tip=0x7f0a0011;
+        public static final int total=0x7f0a0012;
+        public static final int zero=0x7f0a0014;
     }
     public static final class style {
         /** 
