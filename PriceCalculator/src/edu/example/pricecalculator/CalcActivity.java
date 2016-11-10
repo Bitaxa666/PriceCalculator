306dd3f6405ece6a1c04d4e45b2f9ee660b06945
Bitaxa666 <repcity@yandex.ru>
2016-11-10 21:38:46 +0200

    create project

---
+++ PriceCalculator/src/edu/example/pricecalculator/CalcActivity.java
@@ -0,0 +1,19 @@
+package edu.example.pricecalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivity extends Activity 
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+
+  
+    }
+
+
+  
+}
